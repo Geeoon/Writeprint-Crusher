78e6b7f8f97c0c24319f153c2426861f52444ba5
Geeoon Chung <geeoon13@gmail.com>
2024-09-15 16:50:19 -0700

    Print usage

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 #include <unistd.h>
 
+void print_usage(char* name);
+
 int main(int argc, char* argv[]) {
-    // check if being used in pipe
-    
+    bool is_piped{!isatty(STDIN_FILENO)};
+    if (argc < 2) {
+        std::cerr << "No command line arguments supplied." << std::endl;
+        print_usage(argv[0]);
+        return -1;
+    }
+
+    for (int i = 0; i < argc; i++) {
+        
+        std::cout << argv[i] << std::endl;
+    }
     return 0;
+}
+
+void print_usage(char* name) {
+    std::cerr << "Usage: " << name << " [--create-identity name] [-i name] [-f file]" << std::endl;
 }
